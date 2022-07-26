36c0777a3e8caeb1b6a62f96632d5515e5954ffa
rishabhdeepsingh <rishabhdeepsingh98@gmail.com>
2022-07-27 01:59:25 +0530

    Point.h Added methods

--- src/geometry/point.h
+++ src/geometry/point.h
@@ -5,6 +5,7 @@
 #ifndef COMPETITIVE_CPP_SRC_GEOMETRY_POINT_H_
 #define COMPETITIVE_CPP_SRC_GEOMETRY_POINT_H_
 
+#include <type_traits>
 namespace rishabhdeep {
 
 namespace competitive_cpp {
@@ -20,34 +21,33 @@ struct Point {
   explicit Point(T _x, T _y) : x(_x), y{_y} {}
 
   // Addition
-  Point<T> operator+(Point<T>);
+  Point<T> operator+(Point<T>) const;
   Point<T> &operator+=(Point<T>);
   // Subtraction
-  Point<T> operator-(Point<T>);
+  Point<T> operator-(Point<T>) const;
   Point<T> &operator-=(Point<T>);
   // Negative operator
-  Point<T> operator-() {
+  Point<T> operator-() const {
     return Point<T>(-this->x, -this->y);
   }
 
-  template <typename U>
-  auto operator*(U o) -> Point<decltype(x + o)> {
-    return Point<decltype(x + o)>(this->x * o, this->y * o);
-  }
-
+  // Multiplication
+  Point<T> operator*(Point<T>) const;
+  Point<T> &operator*=(Point<T>);
   template <typename U>
   auto operator*=(U o) -> Point<decltype(x * o)> {
     this->x *= o;
     this->y *= o;
     return *this;
   }
+  template <typename U>
+  auto operator*(U o) const -> Point<decltype(x * o)> {
+    return Point<decltype(x * o)>(this->x * o, this->y * o);
+  }
 
-  // Multiplication
-  Point<T> operator*(Point<T>);
-  Point<T> &operator*=(Point<T>);
   // Divide
-  Point<T> operator/(const Point<T>) const;
-  Point<T> &operator/=(const Point<T>);
+  Point<T> operator/(Point<T>) const;
+  Point<T> &operator/=(Point<T>);
   template <typename U>
   auto operator/=(U o) -> Point<decltype(x / o)> {
     this->x /= o;
@@ -56,19 +56,30 @@ struct Point {
   }
 
   // Modulo
-  Point<T> &operator%=(const Point<T>);
-  Point<T> &operator%=(T o);
+  Point<T> operator%(Point<T>) const;
+  Point<T> &operator%=(Point<T>);
+  template <typename U>
+  auto operator%=(U o) -> Point<decltype(x % o)> {
+    this->x %= o;
+    this->y %= o;
+    return *this;
+  }
 
-  static const Point<T> ZERO;
-  static const Point<T> ONE;
-  static const Point<T> TEN;
+  bool operator==(const Point &rhs) const {
+    return this->x == rhs.x and this->y == rhs.y;
+  }
+
+  bool operator!=(const Point &rhs) const {
+    return !(this->x == rhs.x and this->y == rhs.y);
+  }
+
 };
 
+// Addition
 template <typename T>
-Point<T> Point<T>::operator+(Point<T> o) {
+Point<T> Point<T>::operator+(Point<T> o) const {
   return Point<T>(this->x + o.x, this->y + o.y);
 }
-
 template <typename T>
 Point<T> &Point<T>::operator+=(const Point<T> o) {
   this->x += o.x;
@@ -76,34 +87,40 @@ Point<T> &Point<T>::operator+=(const Point<T> o) {
   return *this;
 }
 
+// Negation
 template <typename T>
-Point<T> Point<T>::operator-(Point<T> o) {
+Point<T> Point<T>::operator-(Point<T> o) const {
   return Point<T>(this->x - o.x, this->y - o.y);
 }
-
-template <typename T>
-Point<T> Point<T>::operator*(Point<T> o) {
-  return Point<T>(this->x * o.x, this->y * o.y);
-}
-
+// Subtraction
 template <typename T>
 Point<T> &Point<T>::operator-=(Point<T> o) {
   this->x -= o.x;
   this->y -= o.y;
   return *this;
 }
+
+// Multiplication
 template <typename T>
 Point<T> &Point<T>::operator*=(const Point<T> o) {
   this->x *= o.x;
   this->y *= o.y;
   return *this;
 }
+template <typename T>
+Point<T> Point<T>::operator*(Point<T> o) const {
+  return Point<T>(this->x * o.x, this->y * o.y);
+}
+template <typename T, typename U>
+auto operator*(U o, Point<T> p) -> Point<decltype(p.x * o)> {
+  return Point<decltype(p.x * o)>(p.x * o, p.y * o);
+}
 
+// Division
 template <typename T>
 Point<T> Point<T>::operator/(const Point<T> o) const {
   return Point<T>{this->x / o.x, this->y / o.y};
 }
-
 template <typename T>
 Point<T> &Point<T>::operator/=(const Point<T> o) {
   this->x /= o.x;
@@ -111,10 +128,27 @@ Point<T> &Point<T>::operator/=(const Point<T> o) {
   return *this;
 }
 
-template <typename T, typename U>
-Point<T> operator*(U o, Point<T> p) {
-  return p * o;
+// Modulo
+template <typename T>
+Point<T> Point<T>::operator%(Point<T> o) const {
+  return Point<T>(this->x % o.x, this->y % o.y);
 }
+template <typename T>
+Point<T> &Point<T>::operator%=(Point<T> o) {
+  this->x %= o.x;
+  this->y %= o.y;
+  return *this;
+}
+
+// Constants
+template <typename T>
+static const Point<T> ZERO = Point<T>(0, 0);
+template <typename T>
+static const Point<T> ONE = Point<T>(T{1}, T{1});
+template <typename T>
+static const Point<T> TWO = Point<T>(T{2}, T{2});
+template <typename T>
+static const Point<T> TEN = Point<T>(T{10}, T{10});
 
 } // namespace competitive_cpp
 } // namespace rishabhdeep
